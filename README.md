275eafc3fe37e43a5b1e44ff7ca9d2412d637386
Matthew <nifriththegreat@gmail.com>
2012-12-04 20:34:40 +0000

    Title screen logic mostly done

--- README.md
+++ README.md
@@ -1,4 +1,62 @@
 emond-knights
 =============
-
 An experiment with PyGame
+
+To-dos
+======
+
+Global
+------
+* Error checking (should probably be doing this as I go along)
+
+Game Screen
+-----------
+* Backgrounds
+* Tiles
+* Entities
+* Collision detection
+* Scrolling backgrounds
+
+Loading Screen
+--------------
+* Pretty-ifictation
+
+Title Screen
+------------
+* Buttons (New Game, Quit, etc.)
+* Pretty-ification
+
+Control Flow
+============
+The game's control flow is handled mostly by 'handlers'. A handler contains
+all methods and variables pertinent to a particular context of the game. The
+game loop itself calls the update function of the current handler, and handlers
+can change the current handler.
+
+For instance, before the game context is loaded, we need to load a bunch of assets,
+so we have a loading context that shows an animation while a background thread
+does all the loading and whatnot. Then, once the assets are loaded, the handler
+can be safely switched to the game context.
+
+Inside a handler, the update function works as most normal iterations of a game loop
+do: Draw sprites, do logic.
+
+Example Flows
+-------------
+Game start:
+* Start up game
+* Loading handler loads global assets
+* Switch to title screen handler
+* New game selected, create new loading screen handler for level 1
+* Switch to loading handler, loads level 1 assets, creates handler for level 1
+* Switch to game screen handler
+
+Level completed:
+* Player reaches end of level
+* Find next level data from the Game.levels collection
+* Switch to loading screen handler, load that level's assets, etc..
+* Switch to game screen handler.
+
+Game over:
+* Player loses last life
+* Switch to title screen handler, no loading required?
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame, os, sys
+import pygame, os, sys, copy
 from pygame.locals import *
 from threading import Thread
 
@@ -20,6 +20,10 @@ class Handler:
   def update(self):
     print("Default handler")
     return True
+
+## Loading screen stuff
+def dummyLoadingFunction():
+  pass
 
 class LoadingScreenHandler(Handler):
   def __init__(self, callback, nextHandler=None):
@@ -49,21 +53,72 @@ class LoadingScreenHandler(Handler):
     pygame.display.flip()
     return True
 
+## Menu and Title Screen stuff
+
+class Button:
+  def __init__(self, text, function):
+    self.text = text
+    self.function = function
+
+def quitButtonFunction():
+  Game.run = False
+
+def dummyButtonFunction():
+  pass
+
 class TitleScreenHandler(Handler):
-  def __init__(self):
+  def __init__(self, buttons):
     background = pygame.Surface(Game.screen.get_size())
     background = background.convert()
     background.fill((50,50,50))
     font = pygame.font.Font(None, 36)
-    text = font.render("It's the title screen!", 1, (255,255,255))
-    textpos = text.get_rect(centerx=background.get_width()/2, centery=background.get_height()/2)
+    text = font.render("Emond Knights", 1, (255,255,255))
+    textpos = text.get_rect(centerx=background.get_width()/2, centery=background.get_height()/4)
     background.blit(text, textpos)
     self.background = background
+    self.buttons = buttons[:]
+    self.buttonHighlighted = 0
+    self.buttonOffset = background.get_height() / 2
 
   def update(self):
+    # Draw the background..
     Game.screen.blit(self.background, (0,0))
+    # Now draw the buttons
+    for idx, button in enumerate(self.buttons):
+      fontSize = 24
+      if idx == self.buttonHighlighted:
+        fontSize += 4
+      font = pygame.font.Font(None, fontSize)
+      text = font.render(button.text, 1, (255,255,255))
+      textpos = text.get_rect(centerx=self.background.get_width()/2, centery=self.buttonOffset + idx * 60)
+      Game.screen.blit(text, textpos)
+    # Handle any button presses here: Navigating menu, quitting game, selecting things.
+    for event in pygame.event.get():
+        if event.type == QUIT or \
+            (event.type == KEYDOWN and event.key == K_ESCAPE):
+                Game.run = False
+        elif event.type == KEYDOWN and event.key == K_DOWN:
+          if self.buttonHighlighted + 1 < len(self.buttons):
+            self.buttonHighlighted += 1
+        elif event.type == KEYDOWN and event.key == K_UP:
+          if self.buttonHighlighted - 1 >= 0:
+            self.buttonHighlighted -= 1
+        elif event.type == KEYDOWN and event.key == K_RETURN:
+          self.buttons[self.buttonHighlighted].function()
+
     pygame.display.flip()
     return True
+
+class GameScreenHandler(Handler):
+  # The game proper, if you like. An instance of this is created for each level.
+  # Contains instances for the platforms you can jump on, enemies, etc.
+  # Pass in the level information and whatnot.
+  def __init__(self):
+    pass  
+
+  def update(self):
+
+    pass
 
 class Game:
   # Contains variables that are consistant across all handlers,
@@ -72,13 +127,14 @@ class Game:
   
   # PyGame variables..
   screen = None
+  run = True
 
   # Example variables..
   lives = 0
   score = 0
-
-def dummy_loading():
-  pygame.time.wait(2000)
+  currentLevelIndex = 0
+  levelFlow = ["level01", "level02", "level03"]
+  levelFileMap = {"level01": "data/level01.dat"} #etc
 
 def main():
   # Initialise stuff: Pygame, the clock..
@@ -90,26 +146,22 @@ def main():
   pygame.display.set_caption('Emond Knights')
 
   # Load the loading screen stuff, and set the handler.
-  Game.handler = LoadingScreenHandler(dummy_loading, TitleScreenHandler())
+  newGame = Button("New Game", dummyButtonFunction)
+  exitGame = Button("Exit Game", quitButtonFunction)
+  #Game.handler = LoadingScreenHandler(dummyLoadingFunction, TitleScreenHandler([newGame, exitGame]))
+  
 
-  # Load images inside classes..
+  blackground = pygame.Surface(Game.screen.get_size())
+  blackground = blackground.convert()
+  blackground.fill((0,0,0))
+  blackground.set_alpha(255)
 
   # Let's get in to that main loop!
-  while True:
+  while Game.run:
     # Cap the frame rate.
     clock.tick(60)
 
-    # Let the player quit at any time. Remove this when all the input logic is in.
-    for event in pygame.event.get():
-        if event.type == QUIT or \
-            (event.type == KEYDOWN and event.key == K_ESCAPE):
-                return
-
     # For all the fading and whatnot!
-    blackground = pygame.Surface(Game.screen.get_size())
-    blackground = blackground.convert()
-    blackground.fill((0,0,0))
-    blackground.set_alpha(255)
     Game.screen.blit(blackground, (0,0))
 
     # Run the current handler, which will update the screen, etc..
